9f12b46927eff03703264a0ddd34a6c735e9aae8
18mgdev <m.gonzalez.garcia.2000@gmail.com>
2024-04-08 18:16:56 +0200

    quitar campos principio

--- __main__.py
+++ __main__.py
@@ -30,5 +30,22 @@ for e in m_lasexta.get_news_list():
     
 print(len(all_items))
 
-print(all_items[0]["title"])
-print(all_items[0]["content"])
+print(all_items[3]["title"])
+print(all_items[3]["content"])
+
+import keybert
+
+kw_model = keybert.KeyBERT()
+
+#topics, probs = topic_model.fit_transform([e["content"] for e in all_items])
+kw = kw_model.extract_keywords(all_items[3]["content"])
+
+print("palabras clave del contenido: ", kw)
+kw = kw_model.extract_keywords(all_items[3]["title"])
+
+print("palabras clave del título: ", kw)
+print("categorias: ",all_items[3]["categories"])
+
+"""
+una buena manera para establecer las categorias de una noticia es sacar las kw del content y del titulo y sacar las que coincidan
+"""
