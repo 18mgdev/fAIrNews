6cbb31f2cd961e49b16b40900b8224a429d26f5b
18mgdev <m.gonzalez.garcia.2000@gmail.com>
2024-05-15 16:05:22 +0200

    django project deployed

---
+++ web/controller.py
@@ -0,0 +1,8 @@
+from unittest import loader
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from django.template import loader
+
+#import functions.dirsandfiles
+
+INDEX_NAME = "main.html"
