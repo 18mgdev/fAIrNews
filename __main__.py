e72c4ca2a624c1583302861d9f8a04a4f8175f7e
18mgdev <m.gonzalez.garcia.2000@gmail.com>
2024-05-15 11:28:55 +0200

    eldiario añadido y proyecto estructurado
    se ha añadido m_eldiario y se ha estructurado el proyecto en directorios

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
-import m_20minutos, m_abc, m_cope, m_elconfidencial, m_lasexta
-import sm_elcorreo, sm_elmundo,sm_elpais,sm_elperiodico,sm_lavanguardia,sm_lavozdegalicia
-
+from news_sources import m_20minutos, m_abc, m_cope, m_elconfidencial, m_lasexta, m_eldiario
+from news_sources import sm_elcorreo, sm_elmundo,sm_elpais,sm_elperiodico,sm_lavanguardia,sm_lavozdegalicia
 all_items = []
 for e in m_20minutos.get_news_list():
     all_items.append(e)
@@ -12,7 +11,8 @@ for e in m_elconfidencial.get_news_list():
     all_items.append(e)
 for e in m_lasexta.get_news_list():
     all_items.append(e)
-
+for e in m_eldiario.get_news_list():
+    all_items.append(e)
 for e in sm_elcorreo.get_news_list():
     all_items.append(e)
 for e in sm_elmundo.get_news_list():
@@ -27,122 +27,171 @@ for e in sm_lavozdegalicia.get_news_list():
     all_items.append(e)
 
 
-#****************EXTRAER TOPICS
 
 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from gensim import corpora, models
+import string
+import re
 
-# Preparación de datos
-nltk.download('punkt')
 nltk.download('stopwords')
 
-def preprocess_text(text):
-    tokens = word_tokenize(text.lower())
-    tokens = [word for word in tokens if word.isalpha() and word not in stopwords.words('spanish')]
-    return tokens
+def clean_text(s):
+    s = s.lower()
+    s = re.sub(f'[{re.escape(string.punctuation)}]', '', s)
+    return s
 
-def extract_topics(news_list, num_topics=6):
-    texts = [preprocess_text(news) for news in news_list]
-    dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    lda_model = models.LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
-    topics = lda_model.print_topics(num_words=4)
-    # Asegurarse de que solo se extraigan las palabras, omitiendo las probabilidades
-    topics_list = [" ".join([word for word, _ in lda_model.show_topic(topicid, topn=7)]) for topicid, _ in topics]
-    return lda_model, dictionary, corpus, topics_list
+def remove_stop_words(s):
+    stop_words = set(stopwords.words('spanish'))
+    words = s.split()
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+    return " ".join(filtered_words)
 
+from collections import defaultdict # Utilizar defaultdict para manejar contadores automáticamente
 
-def assign_topic_to_news(news, lda_model, dictionary):
-    processed_news = preprocess_text(news)
-    bow_news = dictionary.doc2bow(processed_news)
-    topics = lda_model.get_document_topics(bow_news)
-    main_topic = max(topics, key=lambda x: x[1])  # Selecciona el tema con la mayor probabilidad
-    return main_topic
+def generate_top_keywords(all_items, num_topics=4):
+    """
+    Genera palabras clave en el campo ['keywords'] para cada noticia en all_items y devuelve un TOP de las palabras clave más frecuentes (dict)
+    """
+    all_texts = [remove_stop_words(clean_text(item["title"] + " " + item["content"])) for item in all_items]
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(all_texts)
+    feature_names = tfidf_vectorizer.get_feature_names_out()
+    
+    all_keywords = defaultdict(int)
+    for idx, item in enumerate(all_items):
+        feature_index = tfidf_matrix[idx,:].nonzero()[1]
+        tfidf_scores = zip(feature_index, [tfidf_matrix[idx, x] for x in feature_index])
+        sorted_items = sorted(tfidf_scores, key=lambda x: x[1], reverse=True)[:num_topics]
+        keywords = [feature_names[i] for i, score in sorted_items]
+        item['keywords'] = keywords
+        for keyword in keywords:
+            all_keywords[keyword] += 1
 
-# Lista de noticias
-news_list = [e["title"] for e in all_items]
-
-# Extracción de los temas y obtención del modelo, diccionario y lista de topics
-lda_model, dictionary, corpus, topics_list = extract_topics(news_list)
-
-# Asignación de temas a cada noticia
-news_topics = [(news, assign_topic_to_news(news, lda_model, dictionary)) for news in news_list]
-
-# Mostrar los topics extraídos
-for idx, topic in enumerate(topics_list):
-    print(f"Topic {idx + 1}: {topic}")
-
+    sorted_keywords = sorted(all_keywords.items(), key=lambda x: x[1], reverse=True)
+    return sorted_keywords
 
 
 
-# Asignar los topics a las noticias
-
-for i in range(len(all_items)):
-    all_items[i]["topic"] = news_topics[i][1][0]
-    all_items[i]["topic_score"] = news_topics[i][1][1]
 
 
-resumenes=[]
-for i in range(len(topics_list)):
-    resumenes.append([])
+from transformers import pipeline
+def summarize_headlines(titulares:list):
 
-for i in range(len(all_items)):
-    resumenes[all_items[i]["topic"]].append(all_items[i])
+    text=""
+    for e in titulares:
+        text+=e+". "
+    # pipeline resumen
+    summarizer = pipeline("summarization", model="google-t5/t5-base")
+    # pipeline traducciones
+    es_en_translator = pipeline("translation", model="Helsinki-NLP/opus-mt-es-en")
+    en_es_translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
 
-#en resumenes hemos agrupado las noticias por topic
+    # Traduce el texto del español al inglés
+    translated_text = es_en_translator(text)[0]['translation_text']
+
+    # Genera el resumen en inglés
+    summary_in_english = summarizer(translated_text, max_length=30, min_length=10, do_sample=False)[0]['summary_text']
+
+    # Traduce el resumen de vuelta al español
+    summary_in_spanish = en_es_translator(summary_in_english)[0]['translation_text']
+    return summary_in_spanish
+
 
 
 import spacy
 import math
-# Suponemos que se carga el modelo en español
-nlp = spacy.load("es_core_news_sm")
-def summarize_article(text, num_sentences=5):
-    # Procesar el texto con el modelo NLP
+
+def summarize_articles(contents:list, num_sentences=5, model_name="es_core_news_sm"):
+    text=""
+    for e in contents:
+        text+=e+". "
+    nlp = spacy.load(model_name)
+
+    #procesa el texto
     doc = nlp(text)
     
     # Identificar y priorizar oraciones basadas en entidades nombradas y otros criterios
     sentences = list(doc.sents)
-    print("num fases ", len(sentences))
     ranked_sentences = sorted(sentences, key=lambda s: len(s.ents), reverse=True)
     
     
-    num_sentences = min(num_sentences, math.ceil(len(ranked_sentences)/3)) #divide entre 3 y redondea hacia arriba
-    print("num frases a devolver ", num_sentences)
-    # Compilar el resumen basado en las oraciones mejor clasificadas
+    num_sentences = max(num_sentences, math.ceil(len(ranked_sentences)/3)) #divide entre 3 y redondea hacia arriba
+    #junta el top de mejores frases
     summary = " ".join([str(sentence) for sentence in ranked_sentences[:num_sentences]])
     
     return summary
 
 
 
-#queda hacer el resumen de cada noticia:
-import random
-def summarize_articles_by_topic(topic_index, articles):
+def generate_summs_json(all_items:list, top_keywords:list, min_rank_keyword=3):
     """
-    Genera un resumen de todas las noticias asociadas a un topic específico.
-
-    Args:
-    - topic_index (int): El índice del topic.
-    - articles (list): Lista de artículos (diccionarios) que incluyen contenido y metadatos.
-
-    Returns:
-    - str: Resumen consolidado de las noticias del topic dado.
+    Genera lista de resumenes en formato JSON
     """
-    # Filtrar artículos por el topic especificado
-    topic_articles = [article for article in articles if article["topic"] == topic_index]
+    final_summs = []
+    for keyword, rank in top_keywords:
+        if rank < min_rank_keyword:
+            break
+        titles = []
+        contents = []
+        referencias=[]
+        imagenes=[]
+        index=0
+        while index<len(all_items) and len(titles) < rank:
+            noticia = all_items[index]
+            if keyword in noticia["keywords"]: #dentro se mete la noticia
+                titles.append(noticia["title"])
+                contents.append(noticia["content"])
+                referencias.append({
+                    "title": noticia["title"],
+                    "link": noticia["link"],
+                    "source": noticia["medio"],
+                    "keywords": noticia["keywords"]
+                })
+                if "enclosure" in noticia and noticia["enclosure"]!={} and "link" in noticia["enclosure"]:
+                    imagenes.append({
+                        "link": noticia["enclosure"]["link"]
+                    })
+                elif "enclosure" in noticia and noticia["enclosure"]!={} and "thumbnail" in noticia["enclosure"]:
+                    imagenes.append({
+                        "link": noticia["enclosure"]["thumbnail"]
+                    })
+            index+=1
 
-    title = random.choice(topic_articles)["title"]
-    # Concatenar el contenido de todos los artículos filtrados
-    full_text = " ".join(article["content"] for article in topic_articles)
-
-    # Utilizar la función de resumen en el texto completo
-    summary = summarize_article(full_text)
+        #se monta el json y se añade a la lista final
+        final_summs.append(
+            {
+                "keyword": keyword,
+                "relevance": rank,
+                "title": summarize_headlines(titles).split(".")[0] if len(summarize_headlines(titles).split("."))>0 else summarize_headlines(titles),
+                "summary": summarize_articles(contents),
+                "references": referencias,
+                "media": imagenes
+            }
+            )
+    return final_summs
     
-    return title, summary
 
-topic_index = 0  # Ajustar según el topic de interés
-title, summary = summarize_articles_by_topic(topic_index, all_items)
-print("Resumen del Topic:",title, "\n", summary)
+import json
+# with open("pruebas/noticias.json", "r", encoding="utf-8") as f:
+#     all_items=json.load(f)
+print("num noticias: ",len(all_items))
+
+top_keywords = generate_top_keywords(all_items, num_topics=4)
+
+
+
+
+for e in top_keywords:
+    print(e) if e[1]>1 else None
+
+final_summs=generate_summs_json(all_items, top_keywords, min_rank_keyword=3)
+
+
+with open("pruebas/summs1.json", "w", encoding="utf-8") as f:
+    json.dump(final_summs, f, indent=4, ensure_ascii=False)
+
+
+
