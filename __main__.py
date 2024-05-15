11f7ff967fdaccbbd9899fb49a0e2b848669012f
18mgdev <m.gonzalez.garcia.2000@gmail.com>
2024-05-15 11:33:42 +0200

    conflictos mergeados

--- __main__.py
+++ __main__.py
@@ -74,6 +74,30 @@ def generate_top_keywords(all_items, num_topics=4):
     return sorted_keywords
 
 
+from sklearn.cluster import KMeans
+
+def cluster_news(all_items, num_clusters=5):
+    # Preparar los textos de las noticias
+    all_texts = [remove_stop_words(clean_text(item["title"] + " " + item["content"])) for item in all_items]
+    
+    # Crear la matriz TF-IDF
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(all_texts)
+    
+    # Aplicar K-means
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    clusters = kmeans.fit_predict(tfidf_matrix)
+    
+    # Asignar el cluster a cada noticia
+    for idx, item in enumerate(all_items):
+        item['cluster'] = clusters[idx]
+
+    # Agrupar noticias por cluster
+    clustered_news = defaultdict(list)
+    for item in all_items:
+        clustered_news[item['cluster']].append(item)
+    
+    return clustered_news
 
 
 
@@ -181,6 +205,17 @@ print("num noticias: ",len(all_items))
 
 top_keywords = generate_top_keywords(all_items, num_topics=4)
 
+org_json=[]
+for k in top_keywords:
+    item={}
+    item["keyword"]=k[0]
+    item["noticias"]=[]
+    for e in all_items:
+        if k[0] in e["keywords"]:
+            item["noticias"].append(e)
+    org_json.append(item)
+with open("pruebas/org_noticias.json", "w", encoding="utf-8") as f:
+    json.dump(org_json, f, indent=4, ensure_ascii=False)
 
 
 
@@ -190,7 +225,7 @@ for e in top_keywords:
 final_summs=generate_summs_json(all_items, top_keywords, min_rank_keyword=3)
 
 
-with open("pruebas/summs1.json", "w", encoding="utf-8") as f:
+with open("pruebas/summs2.json", "w", encoding="utf-8") as f:
     json.dump(final_summs, f, indent=4, ensure_ascii=False)
 
 
